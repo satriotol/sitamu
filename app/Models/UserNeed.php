8c4a62da820379fee3aa47c9f127871da38e7462
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-07-19 14:31:49 +0700

    model migration done

---
+++ app/Models/UserNeed.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserNeed extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'guide_name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
