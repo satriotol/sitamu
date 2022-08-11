7d342c75c4e93ea35d3579a29f5b6c78ba9f9543
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-08-11 11:21:29 +0700

    fixing

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserDetail;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ],'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            if ( ! Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ],'Authentication Failed', 500);
+        }
+    }
+    public function register_post(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'phone' => 'required',
+            'instansi' => 'required',
+        ]);
+        DB::beginTransaction();
+        try {
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+            UserDetail::create([
+                'user_id' => $user->id,
+                'phone' => $data['phone'],
+                'instansi' => $data['instansi'],
+            ]);
+            $role = Role::where('name', 'VISITOR')->first();
+            $user->assignRole($role->id);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
+        return ResponseFormatter::success([
+            'user' => $user
+        ],'Authenticated');
+    }
+}
