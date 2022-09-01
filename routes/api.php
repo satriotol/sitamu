662ea9d32371820e9765f6d251c098c58a7afdff
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-01 11:18:54 +0700

    fixing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\UserVisitorController;
+use App\Http\Controllers\API\UserVisitorController;
 use Illuminate\Support\Facades\Route;
 
 /*
