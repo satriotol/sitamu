69603920997cf9133a7fba141aa3d20d4337b034
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-13 08:16:12 +0700

    FIXING

--- app/Http/Controllers/API/UserVisitorController.php
+++ app/Http/Controllers/API/UserVisitorController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Api\ResponseFormatter;
+use App\Http\Controllers\API\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\SurveyAnswer;
 use App\Models\SurveyQuestion;
