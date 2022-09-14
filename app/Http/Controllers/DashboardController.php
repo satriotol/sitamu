5343a4a5cfa4a819282a32ad292627fe929a178a
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-14 14:01:14 +0700

    fixing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,14 +16,14 @@ class DashboardController extends Controller
     {
         $admin = User::role('ADMIN')->get()->count();
         $user = User::role('VISITOR')->get()->count();
-        $user_need = UserNeed::all()->count();
+        $user_need = UserNeed::where('admin_id', '!=', null)->count();
         $cctv = Cctv::all()->count();
         $surveyQuestions = SurveyQuestion::all();
 
         for ($i = 0; $i < 31; $i++) {
             $params[] = $i + 1;
         }
-        
+
         // $period = CarbonPeriod::create(Carbon::now()->startOfMonth()->format('Y-m-d'), Carbon::now()->endOfMonth()->format('Y-m-d'));
         $period = CarbonPeriod::create(Carbon::now()->startOfMonth()->format('Y-m-d'), Carbon::now()->endOfMonth()->format('Y-m-d'));
         $dates = $period->count();
